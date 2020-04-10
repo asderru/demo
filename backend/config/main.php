150b087e7e3b675a08212b70a9a4a918554daf8d
asd <web@hildus.com>
2020-04-10 18:07:53 +0300

    start

--- backend/config/main.php
+++ backend/config/main.php
@@ -1,94 +1,30 @@
 <?php
-
-use yii\bootstrap4\LinkPager;
-
 $params = array_merge(
-    require(__DIR__ . '/../../common/config/params.php'),
-    require(__DIR__ . '/../../common/config/params-local.php'),
-    require(__DIR__ . '/params.php'),
-    require(__DIR__ . '/params-local.php')
+    require __DIR__ . '/../../common/config/params.php',
+    require __DIR__ . '/../../common/config/params-local.php',
+    require __DIR__ . '/params.php',
+    require __DIR__ . '/params-local.php'
 );
 
 return [
-    'id' => 'панель',
+    'id' => 'app-backend',
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
-    'bootstrap' => [
-        'log',
-        'common\bootstrap\SetUp',
-        'backend\bootstrap\SetUp',
-    ],
-    'container' => [
-        'definitions' => [
-            \yii\widgets\LinkPager::class => LinkPager::class,
-        ],
-    ],
+    'bootstrap' => ['log'],
     'modules' => [],
-    'controllerMap' => [
-        'elfinder' => [
-            'class' => 'mihaildev\elfinder\Controller',
-            'access' => ['@'],
-            'plugin' => [
-                [
-                    'class'=>'\mihaildev\elfinder\plugin\Sluggable',
-                    'lowercase' => true,
-                    'replacement' => '-'
-                ]
-            ],
-            'roots' => [
-                [
-                    'baseUrl' => '@staticHost',
-                    'basePath' => '@staticRoot',
-                    'path' => 'files',
-                    'name' => 'Global',
-                    'plugin' => [
-                        'Sluggable' => [
-                            'lowercase' => false,
-                        ]
-                    ]
-                ],
-            ],
-        ],
-    ],
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-backend',
             'cookieValidationKey' => $params['cookieValidationKey'],
         ],
-        'assetManager' => [
-            'bundles' => [
-                'yii\bootstrap4\BootstrapAsset' => [
-                    'sourcePath' => null,
-                    'baseUrl' => 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1',
-                    'css' => [
-                        'css/bootstrap.min.css'
-                    ],
-                ],
-                'yii\bootstrap4\BootstrapPluginAsset' => [
-                    'sourcePath' => null,
-                    'baseUrl' => 'https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1',
-                    'js' => [
-                        'js/bootstrap.bundle.min.js'
-                    ],
-                ],
-            ],
-        ],
         'user' => [
-            'identityClass' => 'common\auth\Identity',
+            'identityClass' => 'common\models\User',
             'enableAutoLogin' => true,
-            'identityCookie' => [
-                'name' => '_identity',
-                'httpOnly' => true,
-                'domain' => $params['cookieDomain'],
-            ],
-            'loginUrl' => ['auth/login'],
+            'identityCookie' => ['name' => '_identity', 'httpOnly' => true, 'domain'=> 'shop.local'],
         ],
         'session' => [
-            'name' => '_session',
-            'cookieParams' => [
-                'domain' => $params['cookieDomain'],
-                'httpOnly' => true,
-            ],
+            // this is the name of the session cookie used for login on the backend
+            'name' => 'advanced',
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
@@ -102,21 +38,14 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        'backendUrlManager' => require __DIR__ . '/urlManager.php',
-        'frontendUrlManager' => require __DIR__ . '/../../frontend/views/urls/urlManager.php',
-        'urlManager' => function () {
-            return Yii::$app->get('backendUrlManager');
-        },
-    ],
-    'as access' => [
-        'class' => 'yii\filters\AccessControl',
-        'except' => ['auth/login', 'site/error'],
-        'rules' => [
-            [
-                'allow' => true,
-                'roles' => ['admin'],
+        /*
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
             ],
         ],
+        */
     ],
     'params' => $params,
 ];
--- frontend/config/main.php
+++ frontend/config/main.php
@@ -1,84 +1,31 @@
 <?php
 $params = array_merge(
-    require(__DIR__ . '/../../common/config/params.php'),
-    require(__DIR__ . '/../../common/config/params-local.php'),
-    require(__DIR__ . '/params.php'),
-    require(__DIR__ . '/params-local.php')
+    require __DIR__ . '/../../common/config/params.php',
+    require __DIR__ . '/../../common/config/params-local.php',
+    require __DIR__ . '/params.php',
+    require __DIR__ . '/params-local.php'
 );
 
 return [
     'id' => 'app-frontend',
     'basePath' => dirname(__DIR__),
-    'bootstrap' => [
-        'log',
-    ],
-    'layout' => 'main',
+    'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
-    'controllerMap' => [
-        'elfinder' => [
-            'class' => 'mihaildev\elfinder\Controller',
-            'access' => ['@'],
-            'disabledCommands' => ['netmount'], //отключение ненужных команд https://github.com/Studio-42/elFinder/wiki/Client-configuration-options#commands
-
-            'plugin' => [
-                [
-                    'class' => '\mihaildev\elfinder\plugin\Sluggable',
-                    'lowercase' => true,
-                    'replacement' => '-'
-                ]
-            ],
-            'roots' => [
-                [
-                    'baseUrl' => '@staticHost',
-                    'basePath' => '@staticRoot',
-                    'path' => 'files',
-                    'name' => 'Global',
-                    'plugin' => [
-                        'Sluggable' => [
-                            'lowercase' => false,
-                        ]
-                    ]
-                ],
-            ],
-        ],
-    ],
     'components' => [
-        'assetManager' => [
-            'bundles' => [
-                'yii\bootstrap4\BootstrapAsset' => [
-                    'sourcePath' => null,
-                    'baseUrl' => 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1',
-                    'css' => [
-                        'css/bootstrap.min.css'
-                    ],
-                ],
-                'yii\bootstrap4\BootstrapPluginAsset' => [
-                    'sourcePath' => null,
-                    'baseUrl' => 'https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1',
-                    'js' => [
-                        'js/bootstrap.bundle.min.js'
-                    ],
-                ],
-            ],
-        ],
         'request' => [
             'csrfParam' => '_csrf-frontend',
             'cookieValidationKey' => $params['cookieValidationKey'],
         ],
         'user' => [
-            'identityClass' => 'common\auth\Identity',
+            'identityClass' => 'common\models\User',
             'enableAutoLogin' => true,
-            'identityCookie' => ['name' => '_identity', 'httpOnly' => true, 'domain' => $params['cookieDomain']],
-            'loginUrl' => ['auth/auth/login'],
+            'identityCookie' => ['name' => '_identity', 'httpOnly' => true, 'domain'=> 'shop.local'],
         ],
-        'session'            => [
-            'name' => '_session',
-            'cookieParams' => [
-                'domain' => $params['cookieDomain'],
-                'httpOnly' => true,
-            ],
+        'session' => [
+            // this is the name of the session cookie used for login on the frontend
+            'name' => 'advanced',
         ],
-        'log'                => [
+        'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
             'targets' => [
                 [
@@ -87,21 +34,17 @@ return [
                 ],
             ],
         ],
-        'view' => [
-            'class' => '\ogheo\htmlcompress\View',
-            'compress' => YII_ENV_DEV ? false : true,
-            // ...
-        ],
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        'frontendUrlManager' => require __DIR__ . '/../views/urls/urlManager.php',
-        'urlManager' => function () {
-            return Yii::$app->get('frontendUrlManager');
-        },
-        'cart' => [
-            'class' => 'yz\shoppingcart\ShoppingCart',
+        /*
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+            ],
         ],
+        */
     ],
     'params' => $params,
 ];
